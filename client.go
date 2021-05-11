7a1ff60f301c28f172a4c4f29313bf12bfc125d0
Ash Keel <ashlordkelly@bulge.exposed>
2021-05-11 12:18:32 +0200

    check in

---
+++ client.go
@@ -0,0 +1,294 @@
+package kvclient
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/sirupsen/logrus"
+
+	kv "github.com/strimertul/kilovolt/v3"
+)
+
+var (
+	ErrNotAuthenticated     = errors.New("not authenticated")
+	ErrSubscriptionNotFound = errors.New("subscription not found")
+)
+
+type Client struct {
+	Endpoint string
+	Logger   logrus.FieldLogger
+
+	headers       http.Header
+	ws            *websocket.Conn
+	mu            sync.Mutex // Used to avoid concurrent writes to socket
+	requests      map[string]chan<- []byte
+	subscriptions map[string][]chan<- string
+}
+
+type ClientOptions struct {
+	Headers http.Header
+	Logger  logrus.FieldLogger
+}
+
+func NewClient(endpoint string, options ClientOptions) (*Client, error) {
+	if options.Logger == nil {
+		options.Logger = logrus.New()
+	}
+
+	client := &Client{
+		Endpoint:      endpoint,
+		Logger:        options.Logger,
+		headers:       options.Headers,
+		ws:            nil,
+		mu:            sync.Mutex{},
+		requests:      make(map[string]chan<- []byte),
+		subscriptions: make(map[string][]chan<- string),
+	}
+
+	err := client.ConnectToWebsocket()
+
+	return client, err
+}
+
+func (s *Client) Close() {
+	if s.ws != nil {
+		s.ws.Close()
+	}
+}
+
+func (s *Client) ConnectToWebsocket() error {
+	uri, err := url.Parse(s.Endpoint)
+	if err != nil {
+		return err
+	}
+	if uri.Scheme == "https" {
+		uri.Scheme = "wss"
+	} else {
+		uri.Scheme = "ws"
+	}
+
+	s.ws, _, err = websocket.DefaultDialer.Dial(uri.String(), s.headers)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		s.Logger.Debug("connected to ws, reading")
+		for {
+			mtype, message, err := s.ws.ReadMessage()
+			if err != nil {
+				s.Logger.WithError(err).Error("websocket read error")
+				return
+			}
+			if mtype != websocket.TextMessage {
+				continue
+			}
+
+			var response kv.Response
+			err = jsoniter.ConfigFastest.Unmarshal(message, &response)
+			if err != nil {
+				s.Logger.WithError(err).Error("websocket deserialize error")
+				return
+			}
+			// Check message
+			if response.RequestID != "" {
+				// We have a request ID, send byte chunk over to channel
+				if chn, ok := s.requests[response.RequestID]; ok {
+					chn <- message
+				} else {
+					s.Logger.WithField("rid", response.RequestID).Error("received response for unknown RID")
+				}
+			} else {
+				// Might be a push
+				switch response.CmdType {
+				case "push":
+					var push kv.Push
+					err = jsoniter.ConfigFastest.Unmarshal(message, &push)
+					if err != nil {
+						s.Logger.WithError(err).Error("websocket deserialize error")
+						continue
+					}
+					// Deliver to subscriptions
+					for sub, chans := range s.subscriptions {
+						if push.Key != sub {
+							continue
+						}
+
+						for _, chann := range chans {
+							chann <- push.NewValue
+						}
+					}
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (s *Client) GetKey(key string) (string, error) {
+	resp, err := s.makeRequest(kv.Request{
+		CmdName: kv.CmdReadKey,
+		Data: map[string]interface{}{
+			"key": key,
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+	return resp.Data.(string), nil
+}
+
+func (s *Client) GetJSON(key string, dst interface{}) error {
+	resp, err := s.makeRequest(kv.Request{
+		CmdName: kv.CmdReadKey,
+		Data: map[string]interface{}{
+			"key": key,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return jsoniter.ConfigFastest.UnmarshalFromString(resp.Data.(string), dst)
+}
+
+func (s *Client) SetKey(key string, data string) error {
+	_, err := s.makeRequest(kv.Request{
+		CmdName: kv.CmdWriteKey,
+		Data: map[string]interface{}{
+			"key":  key,
+			"data": data,
+		},
+	})
+
+	return err
+}
+
+func (s *Client) SetJSON(key string, data interface{}) error {
+	serialized, err := jsoniter.ConfigFastest.MarshalToString(data)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.makeRequest(kv.Request{
+		CmdName: kv.CmdReadKey,
+		Data: map[string]interface{}{
+			"key":  key,
+			"data": serialized,
+		},
+	})
+
+	return err
+}
+
+func (s *Client) Subscribe(key string) (chan string, error) {
+	chn := make(chan string)
+
+	subs, ok := s.subscriptions[key]
+
+	needsAPISubscription := !ok || len(subs) < 1
+	s.subscriptions[key] = append(subs, chn)
+
+	var err error
+	// If this is the first time we subscribe to this key, ask server to push updates
+	if needsAPISubscription {
+		_, err = s.makeRequest(kv.Request{
+			CmdName: kv.CmdSubscribeKey,
+			Data: map[string]interface{}{
+				"key": key,
+			},
+		})
+	}
+
+	return chn, err
+}
+
+func (s *Client) Unsubscribe(key string, chn chan string) error {
+	if _, ok := s.subscriptions[key]; !ok {
+		return nil
+	}
+
+	found := false
+	for idx, sub := range s.subscriptions[key] {
+		if sub == chn {
+			s.subscriptions[key] = append(s.subscriptions[key][:idx], s.subscriptions[key][idx+1:]...)
+			found = true
+		}
+	}
+
+	if !found {
+		return ErrSubscriptionNotFound
+	}
+
+	// If we removed all subscribers, ask server to not push updates to us anymore
+	if len(s.subscriptions[key]) < 1 {
+		_, err := s.makeRequest(kv.Request{
+			CmdName: kv.CmdUnsubscribeKey,
+			Data: map[string]interface{}{
+				"key": key,
+			},
+		})
+		return err
+	}
+
+	return nil
+}
+
+func (s *Client) makeRequest(request kv.Request) (kv.Response, error) {
+	rid := ""
+	for {
+		rid = fmt.Sprintf("%x", rand.Int63())
+		if _, ok := s.requests[rid]; ok {
+			continue
+		}
+		break
+	}
+
+	responseChannel := make(chan []byte)
+	s.requests[rid] = responseChannel
+
+	request.RequestID = rid
+	err := s.send(request)
+	if err != nil {
+		return kv.Response{}, err
+	}
+
+	// Wait for reply
+	message := <-responseChannel
+
+	var response kv.Response
+	err = jsoniter.ConfigFastest.Unmarshal(message, &response)
+
+	if !response.Ok {
+		var resperror kv.Error
+		err = jsoniter.ConfigFastest.Unmarshal(message, &resperror)
+		if err != nil {
+			return kv.Response{}, err
+		}
+		return kv.Response{}, fmt.Errorf("%s: %s", resperror.Error, resperror.Details)
+	}
+
+	return response, err
+}
+
+func (s *Client) send(v interface{}) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	w, err := s.ws.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return err
+	}
+	err1 := jsoniter.ConfigFastest.NewEncoder(w).Encode(v)
+	err2 := w.Close()
+	if err1 != nil {
+		return err1
+	}
+	return err2
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/strimertul/kilovolt-client-go
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/json-iterator/go v1.1.11
+	github.com/sirupsen/logrus v1.8.1
+	github.com/strimertul/kilovolt/v3 v3.0.0
+)
